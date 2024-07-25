7295efea391d2ec78ace94e7370ac02b08ed1226
zach <zwright5270@gmail.com>
2024-07-24 17:49:55 -0700

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+// config.js
+export const PORT = 3000;
+export const mongoUrl = 'mongodb://localhost:27017/BookStoreDB';
---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { PORT, mongoUrl } from './config.js';
+
+//Initialize app
+const app = express()
+mongoose.connect(mongoUrl)
+    .then(
+        app.listen(PORT, () => {console.log(`App is live on port ${PORT}`)}),
+        console.log("Connected to DB")
+    )
+    .catch(() => {
+        console.log("Error occured connecting to database!")
+    })
+    
